cb58d412f3f70a3aebd444b1ba103314b0757b79
BirdAPI <birdapi@gmail.com>
2013-09-10 21:47:35 -0700

    More refactoring, moved the xnl functions into a separate file.

--- yamaha.py
+++ yamaha.py
@@ -2,71 +2,11 @@ __author__ = 'Anthony Casagrande'
 
 # Python Imports
 from xml.dom import minidom
-import httplib
 
 # Local Imports
 import globals
 from helpers import *
-
-def do_xml(xml, timeout=None, ip=None, port=None, return_result=False):
-    '''
-    Base function to send/receive xml using either GET or POST
-    '''
-    if ip is None:
-        ip = globals.ip_address
-    if port is None:
-        port = globals.port
-    if timeout is None:
-        conn = httplib.HTTPConnection('{0}:{1}'.format(ip, port), timeout=float(globals.default_timeout))
-    else:
-        conn = httplib.HTTPConnection('{0}:{1}'.format(ip, port), timeout=float(timeout))
-    headers = { "Content-type": "text/xml" }
-    conn.request("POST", "/YamahaRemoteControl/ctrl", "", headers)
-    conn.send(xml)
-    if return_result:
-        response = conn.getresponse()
-        rval = response.read()
-        conn.close()
-        return rval
-    else:
-        conn.close()
-        return True
-
-def send_xml(xml, timeout=None, ip=None, port=None):
-    do_xml(xml, timeout, ip, port, return_result=False)
-
-def put_xml(xml, timeout=None, ip=None, port=None):
-    send_xml('<YAMAHA_AV cmd="PUT">{0}</YAMAHA_AV>'.format(xml), timeout, ip, port)
-
-def zone_put_xml(zone, xml, timeout=None, ip=None, port=None):
-    if zone < 2:
-        put_xml('<Main_Zone>{0}</Main_Zone>'.format(xml), timeout, ip, port)
-    else:
-        put_xml('<Zone_{1}>{0}</Zone_{1}>'.format(xml, zone), timeout, ip, port)
-
-def receive_xml(xml, timeout=None, ip=None, port=None):
-    return do_xml(xml, timeout, ip, port, return_result=True)
-
-def get_xml(xml, timeout=None, ip=None, port=None):
-    return receive_xml('<YAMAHA_AV cmd="GET">{0}</YAMAHA_AV>'.format(xml), timeout, ip, port)
-
-def zone_get_xml(zone, xml, timeout=None, ip=None, port=None):
-    if zone < 2:
-        get_xml('<Main_Zone>{0}</Main_Zone>'.format(xml), timeout, ip, port)
-    else:
-        get_xml('<Zone_{1}>{0}</Zone_{1}>'.format(xml, zone), timeout, ip, port)
-
-def get_basic_status(zone=0, timeout=None, ip=None, port=None):
-    return zone_get_xml(zone, '<Basic_Status>GetParam</Basic_Status>', timeout, ip, port)
-
-def get_tuner_status(timeout=None, ip=None, port=None):
-    return get_xml('<Tuner><Play_Info>GetParam</Play_Info></Tuner>', timeout, ip, port)
-
-def get_tuner_presets(timeout=None, ip=None, port=None):
-    return get_xml('<Tuner><Play_Control><Preset><Data>GetParam</Data></Preset></Play_Control></Tuner>', timeout, ip, port)
-
-def get_config(timeout=None, ip=None, port=None):
-    return get_xml('<System><Config>GetParam</Config></System>', timeout, ip, port)
+from yamaha_xml import *
 
 def increase_volume(zone=0, inc=0.5):
     zone_put_xml(zone, '<Volume><Lvl><Val>{0}</Val><Exp>1</Exp><Unit>dB</Unit></Lvl></Volume>'.format(int(get_volume() + 10.0 * inc)))
@@ -78,7 +18,7 @@ def change_volume(zone=0, diff=0.0):
     zone_put_xml(zone, '<Volume><Lvl><Val>{0}</Val><Exp>1</Exp><Unit>dB</Unit></Lvl></Volume>'.format(int(get_volume() + 10.0 * diff)))
 
 def get_volume():
-    return get_int_param('Val')
+    return get_status_int('Val')
 
 def set_volume(zone=0, value=-25.0):
     zone_put_xml(zone, '<Volume><Lvl><Val>{0}</Val><Exp>1</Exp><Unit>dB</Unit></Lvl></Volume>'.format(int(value * 10.0)))
@@ -90,7 +30,7 @@ def mute_off(zone=0):
     zone_put_xml(zone, '<Volume><Mute>Off</Mute></Volume>')
 
 def get_mute(zone=0):
-    return get_is_param_on('Mute')
+    return get_status_param_is_on('Mute', zone)
 
 def power_on(zone=0):
     zone_put_xml(zone, '<Power_Control><Power>On</Power></Power_Control>')
@@ -126,44 +66,19 @@ def toggle_straight_decode(zone=0):
         straight(zone)
 
 def get_straight(zone=0):
-    return get_is_param_on('Straight')
+    return get_status_param_is_on('Straight', zone)
 
 def set_enhancer(arg, zone=0):
     zone_put_xml(zone, '<Surround><Program_Sel><Current><Enhancer>{0}</Enhancer></Current></Program_Sel></Surround>'.format(arg))
 
 def get_enhancer(zone=0):
-    return get_is_param_on('Enhancer')
+    return get_status_param_is_on('Enhancer', zone)
 
 def get_sound_program_name():
-    return get_string_param('Sound_Program')
+    return get_status_string('Sound_Program')
 
 def get_source_number():
-    return get_int_param('Src_Number')
-
-def get_is_param_on(param):
-    return get_string_param(param) == "On"
-
-def get_int_param(param):
-    return int(get_string_param(param))
-
-def get_string_param(param, xml=None):
-    if xml is None:
-        xml = get_basic_status()
-    xmldoc = minidom.parseString(xml)
-    value = xmldoc.getElementsByTagName(param)[0].firstChild.data
-    return value
-
-def get_is_tuner_param_on(param):
-    return get_string_tuner_param(param) == "On"
-
-def get_int_tuner_param(param):
-    return int(get_string_tuner_param(param))
-
-def get_string_tuner_param(param):
-    xml = get_tuner_status()
-    xmldoc = minidom.parseString(xml)
-    value = xmldoc.getElementsByTagName(param)[0].firstChild.data
-    return value
+    return get_status_int('Src_Number')
 
 def toggle_enhancer():
     if get_enhancer():
@@ -183,8 +98,8 @@ def set_source_number(num, zone=0):
 def set_sleep(arg, zone=0):
     zone_put_xml(zone, '<Power_Control><Sleep>On</Sleep></Power_Control>')
 
-def toggle_sleep():
-    if (get_is_param_on('Sleep')):
+def toggle_sleep(zone=0):
+    if (get_status_param_is_on('Sleep', zone)):
         set_sleep('Off')
     else:
         set_sleep('On')
@@ -193,7 +108,7 @@ def set_radio_preset(preset):
     put_xml('<Tuner><Play_Control><Preset><Preset_Sel>{0}</Preset_Sel></Preset></Play_Control></Tuner>'.format(preset))
 
 def get_radio_band():
-    return get_string_tuner_param('Band')
+    return get_tuner_string('Band')
 
 def toggle_radio_amfm():
     if get_radio_band() == 'FM':
@@ -205,7 +120,7 @@ def set_radio_band(band):
     put_xml('<Tuner><Play_Control><Tuning><Band>{0}</Band></Tuning></Play_Control></Tuner>'.format(band))
 
 def modify_radio_preset(diff, turn_on, wrap):
-    oldpreset = get_int_tuner_param('Preset_Sel')
+    oldpreset = get_tuner_int('Preset_Sel')
     preset = oldpreset + diff
     set_radio_preset(preset)
     if turn_on:
@@ -221,18 +136,6 @@ def modify_radio_preset(diff, turn_on, wrap):
             preset = count
             set_radio_preset(preset)
 
-def is_radio_on():
-    return get_string_param('Input_Sel') == "TUNER"
-
-def auto_radio_freq(updown):
-    put_xml('<Tuner><Play_Control><Auto_Freq>{0}</Auto_Freq></Play_Control></Tuner>'.format(updown))
-
-def manual_radio_freq(updown):
-    put_xml('<Tuner><Play_Control><Tuning><Freq>{0}</Freq></Tuning></Play_Control></Tuner>'.format(updown))
-
-def set_radio_freq(freq):
-    print "Not implemented!"
-
 def get_radio_preset_count():
     xml = get_tuner_presets()
     xmldoc = minidom.parseString(xml)
@@ -246,6 +149,18 @@ def get_radio_preset_count():
         else:
             done = True
     return count
+
+def is_radio_on():
+    return get_status_string('Input_Sel') == "TUNER"
+
+def auto_radio_freq(updown):
+    put_xml('<Tuner><Play_Control><Auto_Freq>{0}</Auto_Freq></Play_Control></Tuner>'.format(updown))
+
+def manual_radio_freq(updown):
+    put_xml('<Tuner><Play_Control><Tuning><Freq>{0}</Freq></Tuning></Play_Control></Tuner>'.format(updown))
+
+def set_radio_freq(freq):
+    print "Not implemented!"
 
 def set_scene(scene_num, zone=0):
     zone_put_xml(zone, '<Scene><Scene_Sel>Scene {0}</Scene_Sel></Scene>'.format(scene_num))
---
+++ yamaha_xml.py
@@ -0,0 +1,108 @@
+__author__ = 'Anthony Casagrande'
+
+# Python Imports
+from xml.dom import minidom
+import httplib
+
+# Local Imports
+import globals
+from helpers import *
+
+def do_xml(xml, timeout=None, ip=None, port=None, return_result=False):
+    '''
+    Base function to send/receive xml using either GET or POST
+    '''
+    if ip is None:
+        ip = globals.ip_address
+    if port is None:
+        port = globals.port
+    if timeout is None:
+        conn = httplib.HTTPConnection('{0}:{1}'.format(ip, port), timeout=float(globals.default_timeout))
+    else:
+        conn = httplib.HTTPConnection('{0}:{1}'.format(ip, port), timeout=float(timeout))
+    headers = { "Content-type": "text/xml" }
+    conn.request("POST", "/YamahaRemoteControl/ctrl", "", headers)
+    conn.send(xml)
+    if return_result:
+        response = conn.getresponse()
+        rval = response.read()
+        conn.close()
+        return rval
+    else:
+        conn.close()
+        return True
+
+def send_xml(xml, timeout=None, ip=None, port=None):
+    '''
+    Communicate with the receiver, but do not wait or return the results
+    '''
+    do_xml(xml, timeout, ip, port, return_result=False)
+
+def put_xml(xml, timeout=None, ip=None, port=None):
+    send_xml('<YAMAHA_AV cmd="PUT">{0}</YAMAHA_AV>'.format(xml), timeout, ip, port)
+
+def zone_put_xml(zone, xml, timeout=None, ip=None, port=None):
+    if zone < 2:
+        put_xml('<Main_Zone>{0}</Main_Zone>'.format(xml), timeout, ip, port)
+    else:
+        put_xml('<Zone_{1}>{0}</Zone_{1}>'.format(xml, zone), timeout, ip, port)
+
+def receive_xml(xml, timeout=None, ip=None, port=None):
+    return do_xml(xml, timeout, ip, port, return_result=True)
+
+def get_xml(xml, timeout=None, ip=None, port=None):
+    return receive_xml('<YAMAHA_AV cmd="GET">{0}</YAMAHA_AV>'.format(xml), timeout, ip, port)
+
+def zone_get_xml(zone, xml, timeout=None, ip=None, port=None):
+    if zone < 2:
+        get_xml('<Main_Zone>{0}</Main_Zone>'.format(xml), timeout, ip, port)
+    else:
+        get_xml('<Zone_{1}>{0}</Zone_{1}>'.format(xml, zone), timeout, ip, port)
+
+def get_basic_status(zone=0, timeout=None, ip=None, port=None):
+    return zone_get_xml(zone, '<Basic_Status>GetParam</Basic_Status>', timeout, ip, port)
+
+def get_tuner_status(timeout=None, ip=None, port=None):
+    return get_xml('<Tuner><Play_Info>GetParam</Play_Info></Tuner>', timeout, ip, port)
+
+def get_tuner_presets(timeout=None, ip=None, port=None):
+    return get_xml('<Tuner><Play_Control><Preset><Data>GetParam</Data></Preset></Play_Control></Tuner>', timeout, ip, port)
+
+def get_config(timeout=None, ip=None, port=None):
+    return get_xml('<System><Config>GetParam</Config></System>', timeout, ip, port)
+
+def get_status_string(param, zone=0):
+    xml = get_basic_status(zone)
+    xmldoc = minidom.parseString(xml)
+    value = xmldoc.getElementsByTagName(param)[0].firstChild.data
+    return value
+
+def get_status_param_is_on(param, zone=0):
+    return get_status_string(param, zone) == "On"
+
+def get_status_int(param, zone=0):
+    return int(get_status_string(param, zone))
+
+def get_config_string(param):
+    xml = get_basic_status()
+    xmldoc = minidom.parseString(xml)
+    value = xmldoc.getElementsByTagName(param)[0].firstChild.data
+    return value
+
+def get_config_param_is_on(param):
+    return get_config_string(param) == "On"
+
+def get_config_int(param):
+    return int(get_config_string(param))
+
+def get_tuner_string(param):
+    xml = get_tuner_status()
+    xmldoc = minidom.parseString(xml)
+    value = xmldoc.getElementsByTagName(param)[0].firstChild.data
+    return value
+
+def get_tuner_param_is_on(param):
+    return get_tuner_string(param) == "On"
+
+def get_tuner_int(param):
+    return int(get_tuner_string(param))
