53324310d0e33897492abec9d6839dca708abb0f
BirdAPI <birdapi@gmail.com>
2013-09-10 21:15:34 -0700

    Lots of refactoring for the sending and receiving of xml functions.

--- yamaha.py
+++ yamaha.py
@@ -8,13 +8,10 @@ import httplib
 import globals
 from helpers import *
 
-# Yamaha request xml constants
-BASIC_STATUS_XML = '<YAMAHA_AV cmd="GET"><Main_Zone><Basic_Status>GetParam</Basic_Status></Main_Zone></YAMAHA_AV>'
-TUNER_STATUS_XML = '<YAMAHA_AV cmd="GET"><Tuner><Play_Info>GetParam</Play_Info></Tuner></YAMAHA_AV>'
-TUNER_PRESETS_XML = '<YAMAHA_AV cmd="GET"><Tuner><Play_Control><Preset><Data>GetParam</Data></Preset></Play_Control></Tuner></YAMAHA_AV>'
-CONFIG_XML = '<YAMAHA_AV cmd="GET"><System><Config>GetParam</Config></System></YAMAHA_AV>'
-
-def get_xml(XML, timeout=None, ip=None, port=None):
+def do_xml(xml, timeout=None, ip=None, port=None, return_result=False):
+    '''
+    Base function to send/receive xml using either GET or POST
+    '''
     if ip is None:
         ip = globals.ip_address
     if port is None:
@@ -25,45 +22,60 @@ def get_xml(XML, timeout=None, ip=None, port=None):
         conn = httplib.HTTPConnection('{0}:{1}'.format(ip, port), timeout=float(timeout))
     headers = { "Content-type": "text/xml" }
     conn.request("POST", "/YamahaRemoteControl/ctrl", "", headers)
-    conn.send(XML)
-    response = conn.getresponse()
-    rval = response.read()
-    conn.close()
-    return rval
-
-def get_basic_status(timeout=None):
-    return get_xml(BASIC_STATUS_XML, timeout)
-
-def get_tuner_status(timeout=None):
-    return get_xml(TUNER_STATUS_XML, timeout)
-
-def get_tuner_presets(timeout=None):
-    return get_xml(TUNER_PRESETS_XML, timeout)
-
-def get_config(timeout=None, ip=None):
-    return get_xml(CONFIG_XML, timeout, ip=ip)
-
-def send_xml(XML):
-    conn = httplib.HTTPConnection("%s:%s" % ( globals.ip_address, globals.port ), timeout=float(globals.default_timeout))
-    headers = { "Content-type": "text/xml" }
-    conn.request("POST", "/YamahaRemoteControl/ctrl", "", headers)
-    conn.send(XML)
-    conn.close()
-
-def zone_put_xml(zone, xml):
-    if zone < 2:
-        send_xml('<YAMAHA_AV cmd="PUT"><Main_Zone>{0}</Main_Zone></YAMAHA_AV>'.format(xml))
+    conn.send(xml)
+    if return_result:
+        response = conn.getresponse()
+        rval = response.read()
+        conn.close()
+        return rval
     else:
-        send_xml('<YAMAHA_AV cmd="PUT"><Zone_{1}>{0}</Zone_{1}></YAMAHA_AV>'.format(xml, zone))
+        conn.close()
+        return True
+
+def send_xml(xml, timeout=None, ip=None, port=None):
+    do_xml(xml, timeout, ip, port, return_result=False)
+
+def put_xml(xml, timeout=None, ip=None, port=None):
+    send_xml('<YAMAHA_AV cmd="PUT">{0}</YAMAHA_AV>'.format(xml), timeout, ip, port)
+
+def zone_put_xml(zone, xml, timeout=None, ip=None, port=None):
+    if zone < 2:
+        put_xml('<Main_Zone>{0}</Main_Zone>'.format(xml), timeout, ip, port)
+    else:
+        put_xml('<Zone_{1}>{0}</Zone_{1}>'.format(xml, zone), timeout, ip, port)
+
+def receive_xml(xml, timeout=None, ip=None, port=None):
+    return do_xml(xml, timeout, ip, port, return_result=True)
+
+def get_xml(xml, timeout=None, ip=None, port=None):
+    return receive_xml('<YAMAHA_AV cmd="GET">{0}</YAMAHA_AV>'.format(xml), timeout, ip, port)
+
+def zone_get_xml(zone, xml, timeout=None, ip=None, port=None):
+    if zone < 2:
+        get_xml('<Main_Zone>{0}</Main_Zone>'.format(xml), timeout, ip, port)
+    else:
+        get_xml('<Zone_{1}>{0}</Zone_{1}>'.format(xml, zone), timeout, ip, port)
+
+def get_basic_status(zone=0, timeout=None, ip=None, port=None):
+    return zone_get_xml(zone, '<Basic_Status>GetParam</Basic_Status>', timeout, ip, port)
+
+def get_tuner_status(timeout=None, ip=None, port=None):
+    return get_xml('<Tuner><Play_Info>GetParam</Play_Info></Tuner>', timeout, ip, port)
+
+def get_tuner_presets(timeout=None, ip=None, port=None):
+    return get_xml('<Tuner><Play_Control><Preset><Data>GetParam</Data></Preset></Play_Control></Tuner>', timeout, ip, port)
+
+def get_config(timeout=None, ip=None, port=None):
+    return get_xml('<System><Config>GetParam</Config></System>', timeout, ip, port)
 
 def increase_volume(zone=0, inc=0.5):
-    zone_put_xml(zone, '<Volume><Lvl><Val>{0}</Val><Exp>1</Exp><Unit>dB</Unit></Lvl></Volume>'.format(int(get_volume() + 10 * inc)))
+    zone_put_xml(zone, '<Volume><Lvl><Val>{0}</Val><Exp>1</Exp><Unit>dB</Unit></Lvl></Volume>'.format(int(get_volume() + 10.0 * inc)))
 
 def decrease_volume(zone=0, dec=0.5):
-    zone_put_xml(zone, '<Volume><Lvl><Val>{0}</Val><Exp>1</Exp><Unit>dB</Unit></Lvl></Volume>'.format(int(get_volume() - 10 * dec)))
+    zone_put_xml(zone, '<Volume><Lvl><Val>{0}</Val><Exp>1</Exp><Unit>dB</Unit></Lvl></Volume>'.format(int(get_volume() - 10.0 * dec)))
 
 def change_volume(zone=0, diff=0.0):
-    zone_put_xml(zone, '<Volume><Lvl><Val>{0}</Val><Exp>1</Exp><Unit>dB</Unit></Lvl></Volume>'.format(int(get_volume() + 10 * diff)))
+    zone_put_xml(zone, '<Volume><Lvl><Val>{0}</Val><Exp>1</Exp><Unit>dB</Unit></Lvl></Volume>'.format(int(get_volume() + 10.0 * diff)))
 
 def get_volume():
     return get_int_param('Val')
@@ -71,55 +83,55 @@ def get_volume():
 def set_volume(zone=0, value=-25.0):
     zone_put_xml(zone, '<Volume><Lvl><Val>{0}</Val><Exp>1</Exp><Unit>dB</Unit></Lvl></Volume>'.format(int(value * 10.0)))
 
-def mute_on():
-    send_xml('<YAMAHA_AV cmd="PUT"><Main_Zone><Volume><Mute>On</Mute></Volume></Main_Zone></YAMAHA_AV>')
+def mute_on(zone=0):
+    zone_put_xml(zone, '<Volume><Mute>On</Mute></Volume>')
 
-def mute_off():
-    send_xml('<YAMAHA_AV cmd="PUT"><Main_Zone><Volume><Mute>Off</Mute></Volume></Main_Zone></YAMAHA_AV>')
+def mute_off(zone=0):
+    zone_put_xml(zone, '<Volume><Mute>Off</Mute></Volume>')
 
-def get_mute():
+def get_mute(zone=0):
     return get_is_param_on('Mute')
 
-def power_on():
-    send_xml('<YAMAHA_AV cmd="PUT"><Main_Zone><Power_Control><Power>On</Power></Power_Control></Main_Zone></YAMAHA_AV>')
+def power_on(zone=0):
+    zone_put_xml(zone, '<Power_Control><Power>On</Power></Power_Control>')
 
-def power_off():
-    send_xml('<YAMAHA_AV cmd="PUT"><Main_Zone><Power_Control><Power>Off</Power></Power_Control></Main_Zone></YAMAHA_AV>')
+def power_off(zone=0):
+    zone_put_xml(zone, '<Power_Control><Power>Off</Power></Power_Control>')
 
-def power_standby():
-    send_xml('<YAMAHA_AV cmd="PUT"><Main_Zone><Power_Control><Power>Standby</Power></Power_Control></Main_Zone></YAMAHA_AV>')
+def power_standby(zone=0):
+    zone_put_xml(zone, '<Power_Control><Power>Standby</Power></Power_Control>')
 
-def toggle_on_standby():
-    send_xml('<YAMAHA_AV cmd="PUT"><Main_Zone><Power_Control><Power>On/Standby</Power></Power_Control></Main_Zone></YAMAHA_AV>')
+def toggle_on_standby(zone=0):
+    zone_put_xml(zone, '<Power_Control><Power>On/Standby</Power></Power_Control>')
 
-def toggle_mute():
-    if get_mute():
-        mute_off()
+def toggle_mute(zone=0):
+    if get_mute(zone):
+        mute_off(zone)
     else:
-        mute_on()
+        mute_on(zone)
 
-def change_source(source):
-    send_xml('<YAMAHA_AV cmd="PUT"><Main_Zone><Input><Input_Sel>%s</Input_Sel></Input></Main_Zone></YAMAHA_AV>' % source)
+def change_source(source, zone=0):
+    zone_put_xml(zone, '<Input><Input_Sel>{0}</Input_Sel></Input>'.format(source))
 
-def straight():
-    send_xml('<YAMAHA_AV cmd="PUT"><Main_Zone><Surround><Program_Sel><Current><Straight>On</Straight><Sound_Program>Straight</Sound_Program></Current></Program_Sel></Surround></Main_Zone></YAMAHA_AV>')
+def straight(zone=0):
+    zone_put_xml(zone, '<Surround><Program_Sel><Current><Straight>On</Straight><Sound_Program>Straight</Sound_Program></Current></Program_Sel></Surround>')
 
-def surround_decode():
-    send_xml('<YAMAHA_AV cmd="PUT"><Main_Zone><Surround><Program_Sel><Current><Straight>Off</Straight><Sound_Program>Surround Decoder</Sound_Program></Current></Program_Sel></Surround></Main_Zone></YAMAHA_AV>')
+def surround_decode(zone=0):
+    zone_put_xml(zone, '<Surround><Program_Sel><Current><Straight>Off</Straight><Sound_Program>Surround Decoder</Sound_Program></Current></Program_Sel></Surround>')
 
-def toggle_straight_decode():
-    if get_straight():
-        surround_decode()
+def toggle_straight_decode(zone=0):
+    if get_straight(zone):
+        surround_decode(zone)
     else:
-        straight()
+        straight(zone)
 
-def get_straight():
+def get_straight(zone=0):
     return get_is_param_on('Straight')
 
-def set_enhancer(arg):
-    send_xml('<YAMAHA_AV cmd="PUT"><Main_Zone><Surround><Program_Sel><Current><Enhancer>%s</Enhancer></Current></Program_Sel></Surround></Main_Zone></YAMAHA_AV>' % arg)
+def set_enhancer(arg, zone=0):
+    zone_put_xml(zone, '<Surround><Program_Sel><Current><Enhancer>{0}</Enhancer></Current></Program_Sel></Surround>'.format(arg))
 
-def get_enhancer():
+def get_enhancer(zone=0):
     return get_is_param_on('Enhancer')
 
 def get_sound_program_name():
@@ -165,11 +177,11 @@ def next_source():
 def previous_source():
     set_source_number(get_source_number() - 1)
 
-def set_source_number(num):
-    send_xml('<YAMAHA_AV cmd="PUT"><Main_Zone><Input><Current_Input_Sel_Item><Src_Number>%i</Src_Number></Current_Input_Sel_Item></Input></Main_Zone></YAMAHA_AV>' % num)
+def set_source_number(num, zone=0):
+    zone_put_xml(zone, '<Input><Current_Input_Sel_Item><Src_Number>{0}</Src_Number></Current_Input_Sel_Item></Input>'.format(num))
 
-def set_sleep(arg):
-    send_xml('<YAMAHA_AV cmd="PUT"><Main_Zone><Power_Control><Sleep>On</Sleep></Power_Control></Main_Zone></YAMAHA_AV>')
+def set_sleep(arg, zone=0):
+    zone_put_xml(zone, '<Power_Control><Sleep>On</Sleep></Power_Control>')
 
 def toggle_sleep():
     if (get_is_param_on('Sleep')):
@@ -178,7 +190,7 @@ def toggle_sleep():
         set_sleep('On')
 
 def set_radio_preset(preset):
-    send_xml('<YAMAHA_AV cmd="PUT"><Tuner><Play_Control><Preset><Preset_Sel>%i</Preset_Sel></Preset></Play_Control></Tuner></YAMAHA_AV>' % preset)
+    put_xml('<Tuner><Play_Control><Preset><Preset_Sel>{0}</Preset_Sel></Preset></Play_Control></Tuner>'.format(preset))
 
 def get_radio_band():
     return get_string_tuner_param('Band')
@@ -190,7 +202,7 @@ def toggle_radio_amfm():
         set_radio_band('FM')
 
 def set_radio_band(band):
-    send_xml('<YAMAHA_AV cmd="PUT"><Tuner><Play_Control><Tuning><Band>%s</Band></Tuning></Play_Control></Tuner></YAMAHA_AV>' % band)
+    put_xml('<Tuner><Play_Control><Tuning><Band>{0}</Band></Tuning></Play_Control></Tuner>'.format(band))
 
 def modify_radio_preset(diff, turn_on, wrap):
     oldpreset = get_int_tuner_param('Preset_Sel')
@@ -213,10 +225,10 @@ def is_radio_on():
     return get_string_param('Input_Sel') == "TUNER"
 
 def auto_radio_freq(updown):
-    send_xml('<YAMAHA_AV cmd="PUT"><Tuner><Play_Control><Auto_Freq>%s</Auto_Freq></Play_Control></Tuner></YAMAHA_AV>' % updown)
+    put_xml('<Tuner><Play_Control><Auto_Freq>{0}</Auto_Freq></Play_Control></Tuner>'.format(updown))
 
 def manual_radio_freq(updown):
-    send_xml('<YAMAHA_AV cmd="PUT"><Tuner><Play_Control><Tuning><Freq>%s</Freq></Tuning></Play_Control></Tuner></YAMAHA_AV>' % updown)
+    put_xml('<Tuner><Play_Control><Tuning><Freq>{0}</Freq></Tuning></Play_Control></Tuner>'.format(updown))
 
 def set_radio_freq(freq):
     print "Not implemented!"
@@ -227,7 +239,7 @@ def get_radio_preset_count():
     count = 0
     done = False
     while not done and count <= 40:
-        num = "Number_%s" % (count + 1)
+        num = "Number_{0}".format(count + 1)
         value = xmldoc.getElementsByTagName(num)[0].getElementsByTagName('Status')[0].firstChild.data
         if value == 'Exist':
             count += 1
@@ -235,8 +247,8 @@ def get_radio_preset_count():
             done = True
     return count
 
-def set_scene(scene_num):
-    send_xml('<YAMAHA_AV cmd="PUT"><Main_Zone><Scene><Scene_Sel>Scene %i</Scene_Sel></Scene></Main_Zone></YAMAHA_AV>' % scene_num)
+def set_scene(scene_num, zone=0):
+    zone_put_xml(zone, '<Scene><Scene_Sel>Scene {0}</Scene_Sel></Scene>'.format(scene_num))
 
 def send_code(code):
-    send_xml('<YAMAHA_AV cmd="PUT"><System><Misc><Remote_Signal><Receive><Code>%s</Code></Receive></Remote_Signal></Misc></System></YAMAHA_AV>' % code)
+    put_xml('<System><Misc><Remote_Signal><Receive><Code>{0}</Code></Receive></Remote_Signal></Misc></System>'.format(code))
